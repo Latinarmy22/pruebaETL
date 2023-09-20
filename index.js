f46c9782579849ac4767f9541fa4e3b60a5122c9
Latinarmy22 <radabeca23@gmail.com>
2023-09-20 06:59:50 -0500

    changes

--- index.js
+++ index.js
@@ -14,18 +14,32 @@ fs.createReadStream(sourcePqrs)
     .pipe(csv())
     .on('data', (row) => {
         const [firtsName, lastName] = row.nombreInterpone.split(' ');
+        const subjectValue = row.descripcion.substring(0,50);
         const transformedRow = {
             firts_name: firtsName,
             last_name: lastName,
-            phone: row.telefonoInterpone,
+            company: row.nombre2,
+            phones: row.telefonoInterpone,
             addres: row.direccionInterpone,
             city: row.idCiudadInterpone,
+            // conversations
+            customer_email: row.emailinterpone,
+            subject: subjectValue,
+            preview: row.descripcion,
+            channel: row.id_tipo_medio,
+            // threads
+            from: row.emailinterpone,
         };
         data.push(transformedRow);
     })
     .on('end', () => {
-        const customers = new pgp.helpers.ColumnSet(['first_name', 'last_name', 'phone', 'addres', 'city'], {table: 'costumers'});
+        const customers = new pgp.helpers.ColumnSet(['first_name', 'last_name','company', 'phones', 'addres', 'city'], {table: 'costumers'});
         const insert = pgp.helpers.insert(data, customers);
+
+        const conversations = new pgp.helpers.ColumnSet(['customer_email','subject'], {table: 'conversations'});
+        const insertConversations = pgp.helpers.insert(data, conversations);
+
+        const threads = new pgp.helpers.ColumnSet(['from'], {table: 'threads'})
 
         db.none(insert)
             .then(() => {
